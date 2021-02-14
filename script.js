370d6e5e1af016018f567673fb6a12f165f321c2
ssmeds <stina_smeds@hotmail.com>
2021-02-14 13:27:28 +0100

    finished two types of functions calculating change in different ways

--- script.js
+++ script.js
@@ -25,21 +25,45 @@ button.addEventListener("click", function (e) {
 // Array of denominations for the function
 const values = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 
-// howMuchChange(321, 500);
+// howMuchChange(50, 500);
 
+// Function that calculates how much change, and how many of each denomination
 function howMuchChange(total, payed) {
+	// Emptying the DOM
+	display.innerHTML = "";
+	inputPayed.value = "";
+	inputTotal.value = "";
+
+	// Declaring the change object array
 	let change = {};
+
+	// Declaring what the difference is between total and payed
 	let difference = payed - total;
-	console.log("difference:", difference);
+	display.insertAdjacentHTML("afterbegin", `<p>Du ska få tillbaka <strong>${difference} kr</strong> i följande valörer</p>`);
+	console.log("difference at start:", difference);
+
+	// Looping through all of the denominations
 	for (let i = 0; i < values.length; i++) {
+		// Saving each denomination as "value"
 		let value = values[i];
-		if (value < difference) {
+		// If the value is less or equal to the difference
+		if (value <= difference) {
+			// Add number of denominations to change object array
 			change[value] = Math.floor(difference / value);
-			difference -= values[i];
+			console.log(difference/value);
+			// difference -= values[i] * change[value];
+
+			// Set difference to the new value of the remainder of difference%value
+			difference = difference % value;
+			console.log("difference:", difference);
+			console.log("values[i]:", values[i]);
+			console.log("change[value]:", change[value]);
+			display.insertAdjacentHTML("beforeend", `<p><strong>${value} kr</strong> : ${change[value]} st</p>`);
 		}
 	}
+	display.insertAdjacentHTML("afterbegin", `<p>Du betalade: <strong>${payed} kr</strong></p>`);
+	display.insertAdjacentHTML("afterbegin", `<p>Total summa att betala: <strong>${total} kr</strong></p>`);
 	console.log("change:", change);
-	display.insertAdjacentHTML("afterbegin", change)
 }
 
 // Working function to show how much change of each denomination
